0817f4120fef17e89a49a04886f8a0b1cf395767
郑汉荣 <charlie.zheng@patpat.com>
2020-08-15 17:00:57 +0800

    ?

--- hash.py
+++ hash.py
@@ -4,16 +4,23 @@ ITEM =0x61c88647
 cur = ITEM
 x=[]
 y =[]
+arr =[7,14,5,12,3,10,1,8,15,6,13,4,11,2,9,0]
+arr_len =len(arr)
+arr_index = 0
+print('for begin')
 for i in range(0,330):
     x.append(i)
-    # y .append(i)
-    print(ITEM&15)
+    if  arr[arr_index ] != cur &15:
+        # 没有打印 Error 说明 cur 值累加 ITEM 的过程其实是对 arr 的遍历[笑哭]
+        print('Error')
+    arr_index += 1
+    arr_index %=16
     y .append( cur & 15)
     cur += ITEM
-
-l2=plt.plot(x,y,'g+',label='type2')
-plt.title('The Lasers in Three Conditions')
-plt.xlabel('row')
-plt.ylabel('column')
-plt.legend()
+print('for end')
+l2=plt.plot(x,y,'g+',label='')
+plt.title('')
+plt.xlabel('')
+plt.ylabel('')
+# plt.legend()
 plt.savefig("mygraph.png")
